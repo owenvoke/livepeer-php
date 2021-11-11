69122b386b964f180f6f7bc71cf738c991cb8909
Owen Voke <development@voke.dev>
2021-11-11 09:24:29 +0000

    tests(ingest): add tests

---
+++ tests/Api/IngestTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use OwenVoke\Livepeer\Api\Ingest;
+
+beforeEach(fn() => $this->apiClass = Ingest::class);
+
+it('should get a list of ingest data centers', function () {
+    $expectedArray = [[
+        'ingest' => 'rtmp://lon-rtmp.livepeer.com/live',
+        'playback' => 'https://lon-cdn.livepeer.com/hls',
+        'base' => 'https://lon-cdn.livepeer.com/',
+    ]];
+
+    $api = $this->getApiMock();
+
+    $api->expects($this->once())
+        ->method('get')
+        ->with('/ingest', ['first' => false])
+        ->willReturn($expectedArray);
+
+    expect($api->all())->toBe($expectedArray);
+});
+
+it('should get the closest available ingest data center', function () {
+    $expectedArray = [
+        'ingest' => 'rtmp://lon-rtmp.livepeer.com/live',
+        'playback' => 'https://lon-cdn.livepeer.com/hls',
+        'base' => 'https://lon-cdn.livepeer.com/',
+    ];
+
+    $api = $this->getApiMock();
+
+    $api->expects($this->once())
+        ->method('get')
+        ->with('/ingest')
+        ->willReturn($expectedArray);
+
+    expect($api->closest())->toBe($expectedArray);
+});
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,12 +2,16 @@
 
 declare(strict_types=1);
 
+use OwenVoke\Livepeer\Api\Ingest;
 use OwenVoke\Livepeer\Api\Session;
 use OwenVoke\Livepeer\Api\Stream;
 use OwenVoke\Livepeer\Client;
 
 it('gets instances from the client', function () {
     $client = new Client();
+
+    // Retrieves Ingest instance
+    expect($client->ingest())->toBeInstanceOf(Ingest::class);
 
     // Retrieves Session instance
     expect($client->session())->toBeInstanceOf(Session::class);
